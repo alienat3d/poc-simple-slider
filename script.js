e87dcda5333eb7d405e5f6811d2e573d93e72f81
Al Tsaplin <al.tsaplin@gmail.com>
2024-01-17 13:34:10 +0300

    Done coding baselines

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider'),
+  prevButton = document.querySelector('.prev-button'),
+  nextButton = document.querySelector('.next-button'),
+  slides = Array.from(slider.querySelectorAll('img')),
+  slideCount = slides.length;
+let slideIndex = 0;
+
+console.log(slides);
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
